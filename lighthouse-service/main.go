b381c50d56004caee885ae12abb1f702cd5ddf67
RealAnna <anna.reale@dynatrace.com>
2021-10-11 16:50:48 +0200

    added comment to trigger build
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- lighthouse-service/main.go
+++ lighthouse-service/main.go
@@ -67,6 +67,7 @@ func gotEvent(ctx context.Context, event cloudevents.Event) error {
 	return nil
 }
 
+// storing wait group into context to sync before shutdown
 func getGracefulContext() context.Context {
 
 	ch := make(chan os.Signal, 1)
